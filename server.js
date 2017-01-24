8a76ea2972086c79a2d163ced3c526af5b78e1d2
rghossi <rafael.ghossi@gmail.com>
2017-01-24 00:36:24 -0200

    configuring webpack

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-var express = require('express');
-var stormpath = require('express-stormpath');
+import webpack from 'webpack';
+import config from './webpack.config';
+import express from 'express';
+import stormpath from 'express-stormpath';
 
-var app = express();
+let app = express();
+let compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
 app.use(stormpath.init(app, {
   web: {
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+import path from 'path';
+import webpack from 'webpack';
+
+module.exports = {
+  entry: [
+    './src/app'
+  ],
+  devtool: 'eval-source-map',
+  output: {
+    path: __dirname,
+    filename: 'app.js',
+    publicPath: '/js/'
+  },
+  module: {
+    loaders: []
+  }
+};
